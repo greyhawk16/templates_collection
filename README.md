a0a005bb4304081cd818fb45f7efcb1ca426ff96
양준헌 <greyhawk16@BlackStar-ui-MacBookPro.local>
2024-03-08 07:21:32 +0900

    flask에 nginx를 적용한 예시 추가

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # templates_collection
 템플릿 모음집
+
+1. `flask`: `docker compose`를 이용하여 flask 앱을 배포
+
+2. `nginx-flask`: `docker compose`를 이용하여, NGINX 웹 서버를 적용한 flask 앱을 배포
---
+++ nginx-flask/deploy/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+
+from flask import Flask, render_template
+from detection_module import *
+# from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# client = MongoClient("mongo:27017")
+
+@app.route('/')
+def todo():
+    from_outer_module = print_true()
+    env_from_file = os.getenv("VIRUSTOTAL_API_KEY")
+    return render_template("index.html", var1 = from_outer_module, var2 = env_from_file)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
+
